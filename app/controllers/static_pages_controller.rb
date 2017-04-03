8877d850907122b2f17e93fdab2e57435f9f84f7
CalebHK <pwwang.hk@gmail.com>
2017-04-03 13:43:38 +0000

    use csmp as RSS feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   
   def home
     @account = current_user.accounts.build if logged_in?
-    doc = Nokogiri::XML(open("http://feeds.bbci.co.uk/news/video_and_audio/business/rss.xml#"))
+    doc = Nokogiri::XML(open("http://www.scmp.com/rss/2/feed"))
     results = doc.css('item')
     @news = []
     (0..results.count-1).each do |i|
@@ -10,7 +10,7 @@ class StaticPagesController < ApplicationController
               'description': results[i].at_css('description').content,
               'pubdate': results[i].at_css('pubDate').content,
               'link': results[i].at_css('link').content,
-              'img': results.xpath("//media:thumbnail")[i].attr('url')}
+              'img': results.xpath("//media:content")[i].attr('url')}
       @news << new
     end
   end
