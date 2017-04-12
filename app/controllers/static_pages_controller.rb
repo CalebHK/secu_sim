b541841da2ff51ae5d356bcdb9dac39bf90a55d2
CalebHK <pwwang.hk@gmail.com>
2017-04-12 14:31:29 +0000

    Remove xml reader from static_page controller to StaticPagesHelper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,17 +2,6 @@ class StaticPagesController < ApplicationController
   
   def home
     @account = current_user.accounts.build if logged_in?
-    doc = Nokogiri::XML(open("http://www.scmp.com/rss/2/feed"))
-    results = doc.css('item')
-    @news = []
-    (0..results.count-1).each do |i|
-      new = { 'title': results[i].at_css('title').content,
-              'description': results[i].at_css('description').content,
-              'pubdate': results[i].at_css('pubDate').content,
-              'link': results[i].at_css('link').content,
-              'img': results.xpath("//media:content")[i].attr('url')}
-      @news << new
-    end
   end
 
   def help
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,19 @@
 module StaticPagesHelper
+  
+  def news_feed(url)
+    # create news feed from scmp
+    doc = Nokogiri::XML(open(url))
+    results = doc.css('item')
+    news = []
+    (0..results.count-1).each do |i|
+      new = {}
+      new[:title] = results[i].at_css('title') ? results[i].at_css('title').content : "No Title"
+      new[:description] = results[i].at_css('description') ? results[i].at_css('description').content : "No Description"
+      new[:pubdate] = results[i].at_css('pubDate') ? results[i].at_css('pubDate').content : "No publish date"
+      new[:link] = results[i].at_css('link') ? results[i].at_css('link').content : root_url
+      new[:img] = results[i].at_css('media|content') ? results[i].at_css('media|content').attr('url') : "#"
+      news << new
+    end
+    news
+  end
 end
