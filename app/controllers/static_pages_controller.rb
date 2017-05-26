71ec6694681ee6faf7a62bff8b6d0d5f4d7080fb
CalebHK <pwwang.hk@gmail.com>
2017-05-26 13:16:49 +0000

    make news_feed as an instance variable for home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   
   def home
     @account = current_user.accounts.build if logged_in?
+    @news = news_feed("http://www.scmp.com/rss/2/feed")
   end
 
   def help
